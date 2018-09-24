761f2392e11d1385eb0610566f8ae40ab758e9be
kyleb13 <38119558+kyleb13@users.noreply.github.com>
2018-09-24 10:01:39 -0700

    minor edits

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function startSlideshow(){
-    var slideshow = document.getElementById("slideshow");
-    var filesystem = require('fs');
-    var filenames = filesystem.readdirSync("/slideshowPics");
-    console.debug(filenames);
-}
